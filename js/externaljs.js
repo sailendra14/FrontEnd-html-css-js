2747db19aa61697367c866afb50159cb3c6617f0
sailendra14 <sailendraindia14@gmail.com>
2020-07-23 19:15:49 +0530

    vid 53 complected

---
+++ js/externaljs.js
@@ -0,0 +1,20 @@
+console.log('This is External JavaScript');
+
+function greet (noe, more){
+    console.log(noe + " is a good boy");
+    console.log(more + " "+ noe);
+}
+
+let name = "sai";
+let name1 = "sukraj";
+let name3= "pravesh";
+let name2 = "satyam";
+let name4 = "rahul";
+let pop = "okay bro";
+
+greet(name);                             
+greet(name1);                             
+greet(name2);                             
+greet(name3);                             
+greet(name4);   
+greet(pop);                          
