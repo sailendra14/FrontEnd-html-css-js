3856c4c811200ea9f5c3a21c7c7aea9ea7764b56
sailendra14 <sailendraindia14@gmail.com>
2020-07-26 21:15:14 +0530

    Backend starting

---
+++ nodejs/backend.js
@@ -0,0 +1,22 @@
+/*Author: Sailendra
+Purpose: JavaScript Nodejs
+date: 23/08/2020 */
+
+
+//Virtual Local Server
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+// NOTE: Underscore _ means previous variable
