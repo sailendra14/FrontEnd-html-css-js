745b573bd8b4a4245db82b2ffee37e7854c5df24
sailendra14 <sailendraindia14@gmail.com>
2020-07-28 07:24:14 +0530

    vid 72 complected

--- Express/app.js
+++ Express/app.js
@@ -1,6 +1,22 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const port = 8000;
+const port = 80;
+
+//serving static files 
+app.use('/static', express.static('static'));
+
+// Set the Template Engine
+app.set('view engine', 'pug');
+
+// Set the views Directory  
+app.set('views', path.join(__dirname, 'views'));
+
+//Our pug demo endpoint
+app.get("/demo", (req, res) => {
+    res.status(200).render('demo', { title: 'Hello World', message: "PUG End Point" });
+});
+
 
 app.get("/", (req, res) => {
     res.send("This is my first express app");
