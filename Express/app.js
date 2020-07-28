cc33922ad635f6ec7d32e9d6cc192bfc04aaeb83
sailendra14 <sailendraindia14@gmail.com>
2020-07-28 20:07:06 +0530

    vid 74

--- Express/app.js
+++ Express/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const app = express();
 const port = 80;
 
 
 //EXPRESS SPACIFIC STUFF
 app.use('/static', express.static('static')); //serving static files 
+app.use(express.urlencoded())
 
 
 //PUG SPACIFIC STUFF
@@ -14,13 +16,23 @@ app.set('views', path.join(__dirname, 'views')); // Set the views Directory
 
 // ENDPOINTS
 app.get('/', (res, req) => {
-    // const content = "This is a Description";
     const content = "THIS IS My CONTENT";
     const title = "THIS IS MY TITLE";
     const params = {'content':content, 'title':title};
     req.status(200).render('index.pug', params);
 });
 
+app.post('/', (req, res)=>{
+    name = req.body.name
+    email = req.body.email
+    age = req.body.age
+    
+    let outputtowrite = `The name of the client is ${name}, ${age} years old.`
+    fs.writeFileSync('output.txt', outputtowrite)
+    const params = {'message': 'You form has been submit successfully!'};
+    res.status(200).render('index.pug', params);
+
+});
 
 // STARTING SERVER
 app.listen(port, () => {
