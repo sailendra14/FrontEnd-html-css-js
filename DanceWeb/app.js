aaf2df7a92450f38ff1c117c64fd24cc2da686e9
sailendra14 <sailendraindia14@gmail.com>
2020-07-29 08:53:15 +0530

    vid 75 complected

---
+++ DanceWeb/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 80;
+
+
+//Express specific stuff
+app.use('/static', express.static('static')); //for serving static files
+app.use(express.urlencoded());
+
+//Pug specific stuff
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'templates')); //Set the views directory
+
+//Endpoints
+app.get('/', (req, res) => {
+    const params = {};
+    res.status(200).render('../templates/index.pug', params);
+});
+
+app.listen(port, () => {
+    console.log(`The applecation is started successfully on port number ${port}...`);
+});
