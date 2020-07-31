93b4c75dade547ff5a838e4c821240dd9d5e4759
sailendra14 <sailendraindia14@gmail.com>
2020-07-31 07:08:43 +0530

    contact data save

--- DanceWeb/app.js
+++ DanceWeb/app.js
@@ -1,8 +1,22 @@
+const bodyparser = require('body-parser');
 const express = require('express');
 const path = require('path');
 const app = express();
 const port = 80;
 
+// getting-started-mongos
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/contactDance', { useNewUrlParser: true, useUnifiedTopology: true });
+
+//Defining Mongos Sechema
+const ContactSchema = new mongoose.Schema({
+    name: String,
+    phone: String,
+    email: String,
+    address: String,
+    desc: String
+});
+const Contact = mongoose.model('Contact', ContactSchema);
 
 //Express specific stuff
 app.use('/static', express.static('static')); //for serving static files
@@ -19,8 +33,16 @@ app.get('/', (req, res) => {
 });
 
 app.get('/contact', (req, res) => {
-    const params = {};
-    res.status(200).render('../templates/contact.pug', params);
+    res.status(200).render('../templates/contact.pug');
+});
+
+app.post('/contact', (req, res) => {
+    var myData = new Contact(req.body);
+    myData.save().then(() => {
+        res.send('This data has been saved to the database.');
+    }).catch(() => {
+        res.status(400).send('Item was not to the database.');
+    });
 });
 
 app.listen(port, () => {
